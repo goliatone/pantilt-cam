d175f1de02775603b0a53c29f79e0e8831691fe7
goliatone <burgosemi@gmail.com>
2016-03-26 12:13:30 -0400

    adding servo controller

---
+++ camera/servos.js
@@ -0,0 +1,26 @@
+var servoblaster = require('servoblaster');
+var repl = require('repl');
+var stream = servoblaster.createWriteStream();
+
+module.exports.init = function(){
+    stream.write({ pin: 0, value: '50%' }); // Specify ServoBlaster ID
+    setTimeout(function(){
+        stream.write({ pin: 1, value: '50%' }); // Specify pin number
+    }, 500);
+};
+
+modue.exports.tilt = function(value){
+    stream.write({pin:1, value: value});
+};
+
+module.exports.pan = function(value){
+    stream.write({pin:0, value: value});
+};
+
+module.exports.off = function(){
+    stream.end();
+};
+
+process.on('exit', function(){
+    stream.end();
+});
