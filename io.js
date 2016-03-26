7a92cd3164ffb9d9f87f53df193d795e98663969
goliatone <burgosemi@gmail.com>
2016-03-26 12:15:47 -0400

    io: adding commands

--- io.js
+++ io.js
@@ -1,5 +1,9 @@
 'use strict';
 var io;
+var servos = require('./camera/servos');
+
+servos.init();
+
 module.exports = function(app, config){
     var server = app.server;
     io = require('socket.io')(server);
@@ -11,10 +15,12 @@ module.exports = function(app, config){
 
         socket.on('cam.pan', function(data){
             console.log('Pan', data);
+            servos.pan(data.value);
         });
 
         socket.on('cam.tilt', function(data){
             console.log('Tilt', data);
+            servos.tilt(data.value);
         });
     });
 };
