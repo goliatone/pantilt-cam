1c0be774dc87b589e113ba97eab2f573b3e6498d
goliatone <burgosemi@gmail.com>
2016-03-26 23:48:55 -0400

    app include camera route

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -21,7 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+
+require('./routes/index')(app, {});
+require('./routes/camera')(app, {});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
