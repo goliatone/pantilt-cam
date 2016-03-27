eedbfe1195e2bf1b42c485dadfc5dec3fa40e4e1
goliatone <burgosemi@gmail.com>
2016-03-27 01:00:09 -0400

    routes: camera stop/start

--- routes/camera.js
+++ routes/camera.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var resolve = require('path').resolve;
-var script = resolve(__dirname + '/../bin/start_camera');
+var startCamera = resolve(__dirname + '/../bin/start_camera');
+var stopCamera = resolve(__dirname + '/../bin/stop_camera');
 
 /* GET home page. */
 router.get('/start', function(req, res) {
-    start(script, function(err, stdout, stderr){
+    require('child_process').exec(startCamera, function(err, stdout, stderr){
+        if(err) return res.send({status: false, message: stderr});
+        res.send({status: true, message: stdout});
+    });
+});
+
+router.get('/stop', function(req, res){
+    require('child_process').exec(stopCamera, function(err, stdout, stderr){
         if(err) return res.send({status: false, message: stderr});
         res.send({status: true, message: stdout});
     });
@@ -14,14 +22,3 @@ router.get('/start', function(req, res) {
 module.exports = function(app, config){
     app.use('/camera', router);
 };
-
-function start(script){
-    require('child_process').exec(script,
-      function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-          console.log('exec error: ' + error);
-        }
-    });
-}
