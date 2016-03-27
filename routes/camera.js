8b1a2dd9c4e273a41ddf93827c967e9ee9af7b28
goliatone <burgosemi@gmail.com>
2016-03-27 01:16:40 -0400

    route: camera use camera controller

--- routes/camera.js
+++ routes/camera.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var resolve = require('path').resolve;
-var startCamera = resolve(__dirname + '/../bin/start_camera');
-var stopCamera = resolve(__dirname + '/../bin/stop_camera');
+var camera = require('../camera');
 
 /* GET home page. */
 router.get('/start', function(req, res) {
-    require('child_process').exec(startCamera, function(err, stdout, stderr){
+    camera.start(function(err, stdout, stderr){
         if(err) return res.send({status: false, message: stderr});
         res.send({status: true, message: stdout});
     });
 });
 
 router.get('/stop', function(req, res){
-    require('child_process').exec(stopCamera, function(err, stdout, stderr){
+    camera.stop(function(err, stdout, stderr){
         if(err) return res.send({status: false, message: stderr});
         res.send({status: true, message: stdout});
     });
